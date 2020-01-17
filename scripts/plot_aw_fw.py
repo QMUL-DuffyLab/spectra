b50b2621712c5a2918cd1515d1cbd1f949bea745
cuamll <callum.gray.10@ucl.ac.uk>
2020-01-17 15:27:33 +0000

    Add plot script for A(w) and F(w)

---
+++ scripts/plot_aw_fw.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--aw_file", help="A(w) input file")
+parser.add_argument("-f", "--fw_file", help="F(w) input file")
+parser.add_argument("-o", "--output_file", help="Output file")
+args = parser.parse_args()
+
+aw = np.loadtxt(args.aw_file)
+aw = aw[aw[:, 0].argsort()]
+# fw = np.loadtxt(args.fw_file)
+# fw = fw[fw[:, 0].argsort()]
+
+# testing for comparison
+aw_chris = np.loadtxt("/Users/cgray/Downloads/Duffy_Fitting_Code/A_spec_theor_CHL_RENGER.txt")
+
+fig, ax = plt.subplots()
+plt.grid()
+plt.xlabel('$ \omega $')
+plt.ylabel(r'$ A(\omega) $')
+plt.xlim((-100.0, 100.0))
+plt.plot(aw[:, 0], aw[:, 1])
+plt.plot(aw_chris[:, 0], aw_chris[:, 1])
+plt.savefig(args.output_file)
+plt.close()
