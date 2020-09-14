054db27a3f8e9628cf7860cc4c5268a10664b886
cuamll <callum.gray.10@ucl.ac.uk>
2020-09-14 15:56:24 +0100

    expand fix file script to rename the files properly, create directories, etc. now takes arguments for directory and protein. to do: carotenoid tresp data, zip up raw pdb files and read in from zip instead, cleaner that way

--- scripts/fix_files.py
+++ scripts/fix_files.py
@@ -3,10 +3,21 @@
 import os
 import sys
 import glob
+import argparse
 import numpy as np
 
-# filelist = glob.glob('LHCII' + '/**/*.*', recursive=True)
-filelist = glob.glob('structures/LHCII_PDB' + '/*.csv', recursive=True)
+parser = argparse.ArgumentParser(description="Generate control and osc. strength files")
+parser.add_argument("-i", "--input_dir", default='structures/LHCII',
+        help="Relative path to input directory.")
+parser.add_argument("-p", "--protein", default='LHCII',
+        help="the protein we're looking at ")
+args = parser.parse_args()
+
+# assumes the PDB files we're fixing have filenames in the form
+# R0.frame where R0 is a sequence of digits which represent the
+# residue number from the MD trajectory and .frame is the frame number.
+# this is how I output the files from cpptraj so should be a safe assumption
+filelist = glob.glob(args.input_dir + '/[0-9]*')
 print(filelist)
 
 # want to keep the atom strings so don't loadtxt!
@@ -16,6 +27,30 @@ chl_tresp = np.genfromtxt("in/chl_tresp.dat", comments='#',
         encoding='utf-8', dtype=None)
 cla_tresp = [list(line) for line in zip(*cla_tresp)]
 chl_tresp = [list(line) for line in zip(*chl_tresp)]
+
+if (args.protein) is 'LHCII':
+    pigments = {
+            '1433': 'CHL601',
+            '1434': 'CHL605',
+            '1435': 'CHL606',
+            '1436': 'CHL607',
+            '1437': 'CHL608',
+            '1438': 'CHL609',
+            '1439': 'CLA602',
+            '1440': 'CLA603',
+            '1441': 'CLA604',
+            '1442': 'CLA610',
+            '1443': 'CLA611',
+            '1444': 'CLA612',
+            '1445': 'CLA613',
+            '1446': 'CLA614',
+            '1447': 'LUT620',
+            '1448': 'LUT621',
+            '1449': 'NEX623',
+            '1450': 'XAT622',
+            }
+else:
+    pigments = {}
 
 '''
    there's probably an easier way of doing this but
@@ -31,19 +66,25 @@ chl_dict = dict(zip(chl_tresp[0], chl_tresp[1]))
 
 for item in filelist:
     # same deal - load in the PDB file
-    arr = np.genfromtxt(item, encoding='utf-8', dtype=None)
+    arr = np.genfromtxt(item, encoding='utf-8', dtype=None, skip_footer=2)
     # we'll append each row of x, y, z, partial charge as we go
     temp_list = []
-    print(item)
     # PDB file contains ligand code - use 
     # that to check which tresp data to use
+    output_dir = args.input_dir + '/' + pigments[str(arr[0][4])]
+    os.makedirs(output_dir, exist_ok=True)
+    frame = str(item)[int((str(item)).find(".")) + 1:]
+    output_file = output_dir + '/frame{}.csv'.format(frame)
+    print(output_file)
+
     ligand_code = str(arr[0][3])
     if ligand_code == "CLA":
         tresp_dict = cla_dict
     elif ligand_code == "CHL":
         tresp_dict = chl_dict
     else:
-        raise ValueError("Invalid ligand code: {}".format(ligand_code))
+        continue
+        # raise ValueError("Invalid ligand code: {}".format(ligand_code))
 
     for pdb_line in arr:
         atom_code = pdb_line[2]
@@ -51,15 +92,13 @@ for item in filelist:
         # but i doubt it's worth the effort
         if atom_code in tresp_dict.keys():
             # the 0.001 is because the values are reported as e * 10^3
-            temp_list.append([pdb_line[6], pdb_line[7], 
-                pdb_line[8], 0.001 * float(tresp_dict[atom_code])])
+            temp_list.append([pdb_line[5], pdb_line[6], 
+                pdb_line[7], 0.001 * float(tresp_dict[atom_code])])
         else:
             # any atom not reported in tresp file has zero charge
             temp_list.append([pdb_line[6], pdb_line[7], pdb_line[8], 0.0])
 
     output = np.array(temp_list)
-    print(temp_list)
-    test_output = "{}_test".format(str(item))
     # this fmt call is to make all the data regular, because
     # otherwise it's an absolute nightmare to read in fortran
-    np.savetxt(test_output, output, fmt='%016.8e')
+    np.savetxt(output_file, output, fmt='%016.8e')
