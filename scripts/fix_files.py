35029d6b0ba2f450c9ce3270ce571f4f7a0923db
cuamll <callum.gray.10@ucl.ac.uk>
2020-09-15 17:22:58 +0100

    linting in fix files script

--- scripts/fix_files.py
+++ scripts/fix_files.py
@@ -6,11 +6,13 @@ import glob
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser(description="Generate control and osc. strength files")
+parser = argparse.ArgumentParser(
+         description="Generate control and osc. strength files"
+         )
 parser.add_argument("-i", "--input_dir", default='structures/LHCII',
-        help="Relative path to input directory.")
+                    help="Relative path to input directory.")
 parser.add_argument("-p", "--protein", default='LHCII',
-        help="the protein we're looking at ")
+                    help="the protein we're looking at")
 args = parser.parse_args()
 
 # assumes the PDB files we're fixing have filenames in the form
@@ -21,34 +23,34 @@ filelist = glob.glob(args.input_dir + '/[0-9]*')
 print(filelist)
 
 # want to keep the atom strings so don't loadtxt!
-cla_tresp = np.genfromtxt("in/cla_tresp.dat", comments='#', 
-        encoding='utf-8', dtype=None)
+cla_tresp = np.genfromtxt("in/cla_tresp.dat", comments='#',
+                          encoding='utf-8', dtype=None)
 chl_tresp = np.genfromtxt("in/chl_tresp.dat", comments='#',
-        encoding='utf-8', dtype=None)
+                          encoding='utf-8', dtype=None)
 cla_tresp = [list(line) for line in zip(*cla_tresp)]
 chl_tresp = [list(line) for line in zip(*chl_tresp)]
 
-if (args.protein) is 'LHCII':
+if (args.protein) == 'LHCII':
     pigments = {
-            '1433': 'CHL601',
-            '1434': 'CHL605',
-            '1435': 'CHL606',
-            '1436': 'CHL607',
-            '1437': 'CHL608',
-            '1438': 'CHL609',
-            '1439': 'CLA602',
-            '1440': 'CLA603',
-            '1441': 'CLA604',
-            '1442': 'CLA610',
-            '1443': 'CLA611',
-            '1444': 'CLA612',
-            '1445': 'CLA613',
-            '1446': 'CLA614',
-            '1447': 'LUT620',
-            '1448': 'LUT621',
-            '1449': 'NEX623',
-            '1450': 'XAT622',
-            }
+        '1433': 'CHL601',
+        '1434': 'CHL605',
+        '1435': 'CHL606',
+        '1436': 'CHL607',
+        '1437': 'CHL608',
+        '1438': 'CHL609',
+        '1439': 'CLA602',
+        '1440': 'CLA603',
+        '1441': 'CLA604',
+        '1442': 'CLA610',
+        '1443': 'CLA611',
+        '1444': 'CLA612',
+        '1445': 'CLA613',
+        '1446': 'CLA614',
+        '1447': 'LUT620',
+        '1448': 'LUT621',
+        '1449': 'NEX623',
+        '1450': 'XAT622',
+        }
 else:
     pigments = {}
 
@@ -56,7 +58,7 @@ else:
    there's probably an easier way of doing this but
    basically we turn the tresp data into a dictionary:
    then we can check each line of the PDB file and get the
-   partial charge for the corresponding atom. It's easier 
+   partial charge for the corresponding atom. It's easier
    to do this way because the tresp file doesn't list atoms
    for which the charge is zero, and also the atoms aren't
    listed in the same order.
@@ -69,7 +71,7 @@ for item in filelist:
     arr = np.genfromtxt(item, encoding='utf-8', dtype=None, skip_footer=2)
     # we'll append each row of x, y, z, partial charge as we go
     temp_list = []
-    # PDB file contains ligand code - use 
+    # PDB file contains ligand code - use
     # that to check which tresp data to use
     output_dir = args.input_dir + '/' + pigments[str(arr[0][4])]
     os.makedirs(output_dir, exist_ok=True)
@@ -92,8 +94,8 @@ for item in filelist:
         # but i doubt it's worth the effort
         if atom_code in tresp_dict.keys():
             # the 0.001 is because the values are reported as e * 10^3
-            temp_list.append([pdb_line[5], pdb_line[6], 
-                pdb_line[7], 0.001 * float(tresp_dict[atom_code])])
+            temp_list.append([pdb_line[5], pdb_line[6], pdb_line[7],
+                              0.001 * float(tresp_dict[atom_code])])
         else:
             # any atom not reported in tresp file has zero charge
             temp_list.append([pdb_line[6], pdb_line[7], pdb_line[8], 0.0])
