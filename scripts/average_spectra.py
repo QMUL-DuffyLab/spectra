54948918ad839c6179bd9b184253d286fb3cf9b8
cuamll <callum.gray.10@ucl.ac.uk>
2020-11-04 16:41:01 +0000

    Change the absorption and fluorescence plotting into a function so I can just call it in the averaging script. still not as concise as it could be but a bit better. also print the standard deviation of the maxima - fluorescence still not redshifted

--- scripts/average_spectra.py
+++ scripts/average_spectra.py
@@ -10,6 +10,7 @@ import os
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from plot_aw import plot_aw_fw
 
 parser = argparse.ArgumentParser(description="Generate control and osc. strength files")
 parser.add_argument("-i", "--input_dir", default='out/LHCII',
@@ -25,79 +26,49 @@ aws = np.zeros_like(initial_data)
 fws = np.zeros_like(initial_data)
 jij = np.zeros_like(np.loadtxt("{}/1/J_ij.out".format(args.input_dir)))
 
+num_frames = 1000 # NB: this should probably be calculated
+aw_max = np.zeros(num_frames)
+fw_max = np.zeros(num_frames)
+
 if args.recalc is 1:
     print("Summing A(w) and F(w) per frame")
-    for i in range(1000):
+    for i in range(num_frames):
         if (i % 100) is 0:
             print(".", end='', flush=True)
 
         direc = "{}/{}".format(args.input_dir, i + 1)
-        aws = aws + np.loadtxt("{}/aw.dat".format(direc))
-        fws = fws + np.loadtxt("{}/fw.dat".format(direc))
+        aw_temp = np.loadtxt("{}/aw.dat".format(direc))
+        aw_max[i] = aw_temp[np.argmax(aw_temp[:, 1])][0]
+        fw_temp = np.loadtxt("{}/fw.dat".format(direc))
+        fw_max[i] = fw_temp[np.argmax(fw_temp[:, 1])][0]
+        aws = aws + aw_temp
+        fws = fws + fw_temp
         jij = jij + np.loadtxt("{}/J_ij.out".format(direc))
 
     print("\nDone.")
-    aws = aws / 1000.
-    fws = fws / 1000.
-    jij = jij / 1000.
+    aws = aws / float(num_frames)
+    fws = fws / float(num_frames)
+    jij = jij / float(num_frames)
 else:
     aws = np.loadtxt("{}/aw_average.dat".format(args.input_dir))
     fws = np.loadtxt("{}/fw_average.dat".format(args.input_dir))
     jij = np.loadtxt("{}/jij_average.dat".format(args.input_dir))
 
+np.savetxt("{}/aw_max.dat".format(args.input_dir), aw_max)
+np.savetxt("{}/fw_max.dat".format(args.input_dir), fw_max)
 np.savetxt("{}/aw_average.dat".format(args.input_dir), aws)
 np.savetxt("{}/fw_average.dat".format(args.input_dir), fws)
 np.savetxt("{}/jij_average.dat".format(args.input_dir), jij)
-
-# delete 0 wavenumber rows to prevent divide-by-zero warning
-aws = np.delete(aws, 0, 0)
-fws = np.delete(fws, 0, 0)
-# convert to wavelength (nm)
-aws[:, 0] = 10000000/aws[:, 0]
-fws[:, 0] = 10000000/fws[:, 0]
+print("Standard deviation of A(w) max = {} (cm^{-1})".format(np.std(aw_max)))
+print("Standard deviation of F(w) max = {} (cm^{-1})".format(np.std(fw_max)))
 
 # experimental data: this filename construction's ugly
 if (args.protein is 'LHCII'):
     aw_exp = np.loadtxt("out/{}/aw_exp.dat".format(args.protein), skiprows=1)
     fw_exp = np.loadtxt("out/{}/fw_exp.dat".format(args.protein), skiprows=1)
+else:
+    aw_exp = np.zeros_like(aws)
+    fw_exp = np.zeros_like(aws)
 
-fig, ax = plt.subplots()
-ax.set_xlim([600, 700])
-plt.xlabel(r'Wavelength (nm)')
-plt.ylabel(r'Intensity (abu)')
-plt.plot(aws[:, 0], aws[:, 1]/np.max(aws[:, 1]),
-         label=r'$ A(\omega) $')
-if (args.protein is 'LHCII'):
-    plt.plot(aw_exp[:, 0], aw_exp[:, 1]/np.max(aw_exp[:, 1]),
-             label=r'Experiment')
-
-plt.grid()
-plt.legend()
-plt.tight_layout()
-plt.savefig("{}/aw_average.pdf".format(args.input_dir))
-
-fig, ax = plt.subplots()
-ax.set_xlim([660, 780])
-plt.xlabel(r'Wavelength (nm)')
-plt.ylabel(r'Intensity (abu)')
-plt.plot(fws[:, 0], fws[:, 1]/np.max(fws[:, 1]),
-         label=r'$ F(\omega) $')
-if (args.protein is 'LHCII'):
-    plt.plot(fw_exp[:, 0], fw_exp[:, 1]/np.max(fw_exp[:, 1]),
-             label=r'Experiment')
-
-plt.grid()
-plt.legend()
-plt.tight_layout()
-plt.savefig("{}/fw_average.pdf".format(args.input_dir))
-
-fig, ax = plt.subplots()
-ax.set_xlim([600, 780])
-plt.xlabel(r'Wavelength (nm)')
-plt.ylabel(r'Intensity (abu)')
-plt.plot(aws[:, 0], aws[:, 1], label=r'$ A(\omega) $')
-plt.plot(fws[:, 0], fws[:, 1], label=r'$ F(\omega) $')
-plt.grid()
-plt.legend()
-plt.tight_layout()
-plt.savefig("{}/aw_fw_average.pdf".format(args.input_dir))
+draw_maximums = (True, True, True)
+plot_aw_fw(aws, fws, aw_exp, fw_exp, draw_maximums, args.input_dir)
--- scripts/plot_aw.py
+++ scripts/plot_aw.py
@@ -3,89 +3,113 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
-parser = argparse.ArgumentParser(description="Plot A(w)")
-parser.add_argument("-d", "--dir", default='out/LHCII',
-                    help="directory of A(w) and F(w) data to plot")
-parser.add_argument("-f", "--frame", default=1,
-                    help="MD frame to calculate for - pass 0 to \
-                    loop over all frames")
+def plot_aw_fw(aw, fw, aw_exp, fw_exp, draw_maximums, path):
+    '''
+    Take simulated absorption and fluorescence spectra (aw and fw)
+    along with experimental equivalents if desired; output plots of
+    A(w), F(w) and the pair together. draw_maximums should be a 
+    list or tuple of logicals which are used to determine whether to
+    draw in vertical lines at the maximum in the A(w)/F(w)/combined plots.
+    There's definitely a neater/more pythonic way of doing this but ¯\_(ツ)_/¯
+    '''
+    '''
+    the first four lines lines switch from wavenumbers to a wavelength
+    in nanometres. NB: this is a hacky way of doing it!
+    I deliberately delete the first row, since it contains the
+    point at zero wavenumber. This is because it'll throw a
+    divide-by-zero RuntimeWarning otherwise. this is fine for our
+    purposes here, because we're only showing from ~550-750nm anyway
+    '''
+    aw = np.delete(aw, 0, 0)
+    fw = np.delete(fw, 0, 0)
+    aw[:, 0] = 10000000/aw[:, 0]
+    fw[:, 0] = 10000000/fw[:, 0]
+    # Plot A(\omega)
+    aw_max = aw[np.argmax(aw[:, 1])]
+    print("Max of A(w):     {}".format(aw_max))
+    print("Max of A(w) exp: {}".format(aw_exp[np.argmax(aw_exp[:, 1])]))
 
-args = parser.parse_args()
+    plt.plot(aw[:, 0], aw[:, 1]/max(aw[:, 1]), label=r'$ A(\omega) $')
+    if (draw_maximums[0] is True):
+        plt.axvline(x=aw_max[0], linestyle='dashed', color='k',
+                    label=r'max = $ {:8.3f} $'.format(aw_max[0]))
 
-print("Plotting A(w) and F(w)")
+    # {a/f}w_exp are set to zero below if there's no experimental data
+    if (np.any(aw_exp)):
+        plt.plot(aw_exp[:, 0], aw_exp[:, 1]/max(aw_exp[:, 1]),
+                 label=r'Mancal $ Q_y $')
 
-aw_data = np.loadtxt("{}/aw.dat".format(args.dir))
-fw_data = np.loadtxt("{}/fw.dat".format(args.dir))
+    plt.xlabel(r'Wavelength (nm)')
+    plt.ylabel(r'$ A(\omega) $ (abu)')
+    ax = plt.gca()
+    ax.set_xlim([550, 750])
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("{}/aw.pdf".format(path))
+    plt.close()
 
-if 'LHCII' in args.dir:
-    plot_exp = 1
-    aw_exp = np.loadtxt('out/LHCII/aw_exp.dat', skiprows=1)
-    fw_exp = np.loadtxt('out/LHCII/fw_exp.dat', skiprows=1)
-else:
-    aw_exp = np.zeros_like(aw_data)
-    fw_exp = np.zeros_like(aw_data)
-    plot_exp = 0
+    # Plot F(\omega)
+    fw_max = fw[np.argmax(fw[:, 1])]
+    print("Max of F(w):     {}".format(fw_max))
+    print("Max of F(w) exp: {}".format(fw_exp[np.argmax(fw_exp[:, 1])]))
+    plt.plot(fw[:, 0], fw[:, 1]/max(fw[:, 1]), label=r'$ F(\omega) $')
+    if (draw_maximums[1] is True):
+        plt.axvline(x=fw_max[0], linestyle='dashed', color='k', 
+                    label=r'max = $ {:8.3f} $'.format(fw_max[0]))
 
+    if (np.any(fw_exp)):
+        plt.plot(fw_exp[:, 0], fw_exp[:, 1]/max(fw_exp[:, 1]),
+                 label='Mancal')
 
-'''
-the next two lines switch from wavenumbers to a wavelength
-in nanometres. NB: this is a hacky way of doing it!
-I deliberately delete the first row, since it contains the
-point at zero wavenumber. This is because it'll throw a
-divide-by-zero RuntimeWarning otherwise. this is fine for our
-purposes here, because we're only showing from ~550-750nm anyway
-'''
-aw_data = np.delete(aw_data, 0, 0)
-fw_data = np.delete(fw_data, 0, 0)
-aw_data[:, 0] = 10000000/aw_data[:, 0]
-fw_data[:, 0] = 10000000/fw_data[:, 0]
-print("Max of A(w):     {}".format(aw_data[np.argmax(aw_data[:, 1])]))
-print("Max of A(w) exp: {}".format(aw_exp[np.argmax(aw_exp[:, 1])]))
-plt.plot(aw_data[:, 0], aw_data[:, 1]/max(aw_data[:, 1]),
-         label=r'$ A(\omega) $')
-if (plot_exp):
-    plt.plot(aw_exp[:, 0], aw_exp[:, 1]/max(aw_exp[:, 1]),
-             label=r'Mancal $ Q_y $')
+    plt.xlabel(r'Wavelength (nm)')
+    plt.ylabel(r'$ F(\omega) $ (abu)')
+    ax = plt.gca()
+    ax.set_xlim([600, 850])
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("{}/fw.pdf".format(path))
 
-plt.xlabel(r'Wavelength (nm)')
-plt.ylabel(r'$ A(\omega) $ (abu)')
+    # combined
+    fig, ax = plt.subplots()
+    ax.set_xlim([550, 850])
+    plt.plot(aw[:, 0], aw[:, 1], label=r'$ A(\omega) $')
+    plt.plot(fw[:, 0], fw[:, 1], label=r'$ F(\omega) $')
+    if (draw_maximums[2] is True):
+        plt.axvline(x=aw_max[0], linestyle='dashed', color='k',
+                    label=r' $ A(\omega) $ max = $ {:8.3f} $'.format(aw_max[0]))
+        plt.axvline(x=fw_max[0], linestyle='dashed', color='k',
+                    label=r' $ F(\omega) $ max = $ {:8.3f} $'.format(fw_max[0]))
 
-ax = plt.gca()
-ax.set_xlim([550, 750])
-plt.grid()
-plt.legend()
-plt.tight_layout()
-plt.savefig("{}/aw.pdf".format(args.dir))
-plt.close()
+    plt.xlabel(r'Wavelength (nm)')
+    plt.ylabel(r'Intensity (abu)')
+    plt.grid()
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("{}/aw_fw.pdf".format(path))
 
+if __name__ == '__main__': 
+    parser = argparse.ArgumentParser(description="Plot A(w)")
+    parser.add_argument("-d", "--dir", default='out/LHCII',
+                        help="directory of A(w) and F(w) data to plot")
+    parser.add_argument("-f", "--frame", default=1,
+                        help="MD frame to calculate for - pass 0 to \
+                        loop over all frames")
 
-plt.plot(fw_data[:, 0], fw_data[:, 1]/max(fw_data[:, 1]),
-         label=r'$ F(\omega) $')
-if (plot_exp):
-    plt.plot(fw_exp[:, 0], fw_exp[:, 1]/max(fw_exp[:, 1]),
-             label='Mancal')
+    args = parser.parse_args()
 
-plt.xlabel(r'Wavelength (nm)')
-plt.ylabel(r'$ F(\omega) $ (abu)')
-print("Max of F(w):     {}".format(fw_data[np.argmax(fw_data[:, 1])]))
-print("Max of F(w) exp: {}".format(fw_exp[np.argmax(fw_exp[:, 1])]))
+    print("Plotting A(w) and F(w)")
 
-ax = plt.gca()
-ax.set_xlim([600, 850])
-plt.grid()
-plt.legend()
-plt.tight_layout()
-plt.savefig("{}/fw.pdf".format(args.dir))
+    aw_data = np.loadtxt("{}/aw.dat".format(args.dir))
+    fw_data = np.loadtxt("{}/fw.dat".format(args.dir))
 
-fig, ax = plt.subplots()
-ax.set_xlim([550, 850])
-plt.plot(aw_data[:, 0], aw_data[:, 1],
-         label=r'$ A(\omega) $')
-plt.plot(fw_data[:, 0], fw_data[:, 1],
-         label=r'$ F(\omega) $')
-plt.xlabel(r'Wavelength (nm)')
-plt.ylabel(r'Intensity (abu)')
-plt.grid()
-plt.legend()
-plt.tight_layout()
-plt.savefig("{}/aw_fw.pdf".format(args.dir))
+    if 'LHCII' in args.dir:
+        aw_exp = np.loadtxt('out/LHCII/aw_exp.dat', skiprows=1)
+        fw_exp = np.loadtxt('out/LHCII/fw_exp.dat', skiprows=1)
+    else:
+        aw_exp = np.zeros_like(aw_data)
+        fw_exp = np.zeros_like(aw_data)
+
+    draw_maximums = (True, True, True)
+    plot_aw_fw(aw_data, fw_data, aw_exp, fw_exp, draw_maximums, args.dir)
