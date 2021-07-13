S0-S1 and S0-S2 widths were the wrong way round in vera.def - fixed. also move plots of averages into plot script and start consolidating; more tidying up to do, need to get rid of ipynb

scripts/average_spectra.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 # tested on python 3.7
 """
-For LHCII data only atm - average the A(w) and F(w) data over every snapshot,
-write them out, plot them.
 """
 
 import os
 import argparse
 import subprocess
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from plot_aw import plot_aw_fw
+import seaborn as sns
+from plot import plot_all
 
 parser = argparse.ArgumentParser(description="Generate control and osc. strength files")
 parser.add_argument("-i", "--input_dir", default='out/LHCII',
@@ -204,20 +204,4 @@ np.savetxt("{}/rmsd_std.dat".format(args.input_dir), rmsd_std)
 np.savetxt("{}/lut620len_average.dat".format(args.input_dir), lut620len_avg)
 np.savetxt("{}/lut620len_std.dat".format(args.input_dir), lut620len_std)
 
-# experimental data: this filename construction's ugly
-if (args.protein is 'LHCII'):
-    aw_exp = np.loadtxt("out/{}/aw_exp.dat".format(args.protein), skiprows=1)
-    fw_exp = np.loadtxt("out/{}/fw_exp.dat".format(args.protein), skiprows=1)
-else:
-    aw_exp = np.zeros_like(aws)
-    fw_exp = np.zeros_like(aws)
-
-draw_maximums = (True, True, True)
-plot_aw_fw(aws, fws, aw_exp, fw_exp, draw_maximums, args.input_dir)
-
-fig, ax = plt.subplots()
-plt.title(r'Lifetime: avg = $ {} $, $ \sigma = {} $'.format(avg_tau, np.std(taus[:, 1])))
-plt.xlabel("Frame")
-plt.ylabel(r'$ \left< \tau \right>$')
-plt.plot(taus[:, 0], taus[:, 1])
-plt.savefig("{}/tau.pdf".format(args.input_dir))
+plot_all(args.input_dir)

scripts/plot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import argparse
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
-def plot_aw_fw(aw, fw, aw_exp, fw_exp, draw_maximums, path):
+def plot_aw_fw(path):
     '''
     Take simulated absorption and fluorescence spectra (aw and fw)
     along with experimental equivalents if desired; output plots of
@@ -20,6 +22,9 @@ def plot_aw_fw(aw, fw, aw_exp, fw_exp, draw_maximums, path):
     divide-by-zero RuntimeWarning otherwise. this is fine for our
     purposes here, because we're only showing from ~550-750nm anyway
     '''
+    draw_maximums = (False)
+    aw = np.loadtxt("{}/aw_average.dat".format(path))
+    fw = np.loadtxt("{}/fw_average.dat".format(path))
     aw = np.delete(aw, 0, 0)
     fw = np.delete(fw, 0, 0)
     aw[:, 0] = 10000000/aw[:, 0]
@@ -27,17 +32,11 @@ def plot_aw_fw(aw, fw, aw_exp, fw_exp, draw_maximums, path):
     # Plot A(\omega)
     aw_max = aw[np.argmax(aw[:, 1])]
     print("Max of A(w):     {}".format(aw_max))
-    print("Max of A(w) exp: {}".format(aw_exp[np.argmax(aw_exp[:, 1])]))
 
     plt.plot(aw[:, 0], aw[:, 1]/max(aw[:, 1]), label=r'$ A(\omega) $')
-    if (draw_maximums[0] is True):
+    if (draw_maximums is True):
         plt.axvline(x=aw_max[0], linestyle='dashed', color='k',
                     label=r'max = $ {:8.3f} $'.format(aw_max[0]))
-
-    # {a/f}w_exp are set to zero below if there's no experimental data
-    if (np.any(aw_exp)):
-        plt.plot(aw_exp[:, 0], aw_exp[:, 1]/max(aw_exp[:, 1]),
-                 label=r'Mancal $ Q_y $')
 
     plt.xlabel(r'Wavelength (nm)')
     plt.ylabel(r'$ A(\omega) $ (abu)')
@@ -52,15 +51,10 @@ def plot_aw_fw(aw, fw, aw_exp, fw_exp, draw_maximums, path):
     # Plot F(\omega)
     fw_max = fw[np.argmax(fw[:, 1])]
     print("Max of F(w):     {}".format(fw_max))
-    print("Max of F(w) exp: {}".format(fw_exp[np.argmax(fw_exp[:, 1])]))
     plt.plot(fw[:, 0], fw[:, 1]/max(fw[:, 1]), label=r'$ F(\omega) $')
-    if (draw_maximums[1] is True):
+    if (draw_maximums is True):
         plt.axvline(x=fw_max[0], linestyle='dashed', color='k', 
                     label=r'max = $ {:8.3f} $'.format(fw_max[0]))
-
-    if (np.any(fw_exp)):
-        plt.plot(fw_exp[:, 0], fw_exp[:, 1]/max(fw_exp[:, 1]),
-                 label='Mancal')
 
     plt.xlabel(r'Wavelength (nm)')
     plt.ylabel(r'$ F(\omega) $ (abu)')
@@ -76,7 +70,7 @@ def plot_aw_fw(aw, fw, aw_exp, fw_exp, draw_maximums, path):
     ax.set_xlim([600, 750])
     plt.plot(aw[:, 0], aw[:, 1], label=r'$ A(\omega) $')
     plt.plot(fw[:, 0], fw[:, 1], label=r'$ F(\omega) $')
-    if (draw_maximums[2] is True):
+    if (draw_maximums is True):
         plt.axvline(x=aw_max[0], linestyle='dashed', color='k',
                     label=r' $ A(\omega) $ max = $ {:8.3f} $'.format(aw_max[0]))
         plt.axvline(x=fw_max[0], linestyle='dashed', color='k',
@@ -101,18 +95,185 @@ if __name__ == '__main__':
 
     print("Plotting A(w) and F(w)")
 
-    aw_data = np.loadtxt("{}/aw.dat".format(args.dir))
-    fw_data = np.loadtxt("{}/fw.dat".format(args.dir))
+    plot_aw_fw(args.dir)
 
-    if 'LHCII' in args.dir:
-        aw_exp = np.loadtxt('out/LHCII/aw_exp.dat', skiprows=1)
-        fw_exp = np.loadtxt('out/LHCII/fw_exp.dat', skiprows=1)
-    elif 'NLLZ' in args.dir:
-        aw_exp = np.loadtxt('out/LHCII/aw_exp.dat', skiprows=1)
-        fw_exp = np.loadtxt('out/LHCII/fw_exp.dat', skiprows=1)
+def plot_heatmap(data, out, *args, kwargs={}, plot_kwargs={}, cbar_kwargs={}):
+    '''
+    Easily plot a heatmap with controllable axis labels, tick labels, colour
+    bar details and various other things. Note that you have to construct the
+    kwargs dictionaries yourself and change/delete keys as needed. This is
+    because normally you'd just have one kwargs with all the keyword arguments
+    in it, but I wanted to be able to set plot parameters and colour bar
+    parameters separately, and you can't pull out chunks of the normal kwargs
+    dictionary to pass them to the plot or colour bar as **kwargs.
+    '''
+    fig, ax = plt.subplots(figsize=(12, 12))
+    if 'title' in kwargs:
+        plt.title(kwargs['title'])
+        
+    if 'cmap' in kwargs:
+        cm = kwargs['cmap']
     else:
-        aw_exp = np.zeros_like(aw_data)
-        fw_exp = np.zeros_like(aw_data)
+        cm = "RdBu_r"
+            
+    X, Y = np.meshgrid(np.arange(np.shape(data)[0] + 1), np.arange(np.shape(data)[1] + 1))
+    # - 0.5 for X and Y to centre the data on the ticks
+    im = ax.pcolormesh(X - 0.5, Y - 0.5, data, cmap=cm, ec="#BBBBBB", **plot_kwargs)
+    if 'xlabel' in kwargs:
+        ax.set_xlabel(kwargs['xlabel'])
+    
+    if 'ylabel' in kwargs:
+        ax.set_ylabel(kwargs['ylabel'])
+    
+    if 'ticklabels' in kwargs:
+        kwargs['x_ticklabels'] = kwargs['ticklabels']
+        kwargs['y_ticklabels'] = kwargs['ticklabels']
+    
+    if 'y_ticklabels' in kwargs:
+        ax.set_yticks(np.arange(len(kwargs['y_ticklabels'])))
+        ax.set_yticklabels(kwargs['y_ticklabels'])
+        
+    if 'x_ticklabels' in kwargs:
+        ax.xaxis.set_ticks_position('top')
+        ax.xaxis.set_label_position('top')
+        ax.set_xticks(np.arange(len(kwargs['x_ticklabels'])))
+        ax.set_xticklabels(kwargs['x_ticklabels'])
+        plt.setp(ax.get_xticklabels(), rotation=90, ha="left", va="center", rotation_mode="anchor")
+        
+    cax = plt.axes([0.125, 0.05, 0.775, 0.055])
+    cbar = fig.colorbar(im, cax=cax, orientation='horizontal', **cbar_kwargs)
+            
+    if 'cbar_label' in kwargs:
+        cbar.set_label(kwargs['cbar_label']) 
+        
+    if 'cbar_ticklabels' in kwargs:
+        cbar.ax.set_xticklabels(kwargs['cbar_ticklabels'])
+        
+    fig.savefig(out)
 
-    draw_maximums = (True, True, True)
-    plot_aw_fw(aw_data, fw_data, aw_exp, fw_exp, draw_maximums, args.dir)
+def plot_all(path):
+    plot_aw_fw(path)
+
+    sns.set(font_scale=2) 
+    pigments = ['CHL601', 'CLA602', 'CLA603', 'CLA604', 'CHL605', 'CHL606', 'CHL607', 'CHL608', 'CHL609', 'CLA610', 'CLA611', 'CLA612', 'CLA613', 'CLA614', 'LUT620', 'LUT621']
+    valfmt = "{:3.1f}"
+    valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    taus = np.loadtxt("{}/{}".format(path, "tau.dat"))
+    tau_avg = np.loadtxt("{}/tau_average.dat".format(path))
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.set_title(r'$ \left< \tau \right> = {:8.3f} $ ps, $ \sigma = {:8.3f} $ ps'.format(tau_avg[0], tau_avg[1]))
+    plt.xlabel("Frame")
+    plt.ylabel(r'$ \left< \tau \right>$')
+    plt.plot(taus[:, 0], taus[:, 1])
+    plt.savefig("{}/tau.pdf".format(path))
+
+    ''' lifetime histogram'''
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.hist(taus[:, 1], edgecolor='k')
+    ax.set_xlabel(r' Lifetime $ \tau $')
+    ax.set_ylabel(r'Counts')
+    ax.set_title(r'$ \left< \tau \right> = {:8.3f} $ ps, $ \sigma = {:8.3f} $ ps'.format(tau_avg[0], tau_avg[1]))
+    plt.savefig("{}/tau_hist.pdf".format(path))
+
+    '''620-612 coupling'''
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.plot(np.loadtxt("{}/j620cl.dat".format(path))[:, 2])
+    ax.set_ylabel(r'620-612 coupling $ (cm^{-1}) $')
+    ax.set_xlabel(r'Frame')
+    fig.savefig("{}/j620612.pdf".format(path))
+
+    '''620 squared dipole moment'''
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.plot(np.loadtxt("{}/musq620.dat".format(path)))
+    ax.set_xlabel("Frame")
+    ax.set_ylabel(r'$ \mu^{2}_{\text{LUT620}} $')
+    fig.savefig("{}/musq620.pdf".format(path))
+
+    ''' 
+    big plot with participation / coupling data
+    this plot is an absolute fucking nightmare!!!
+    '''
+    avg_part = np.loadtxt("{}/eig_average.dat".format(path))
+    exc_620_coupling = np.loadtxt("{}/exc_620_average.dat".format(path))
+    eigvals = np.loadtxt("{}/eigvals_average.dat".format(path))
+    eigvals_std = np.loadtxt("{}/eigvals_std.dat".format(path))
+
+    edgecolour='#BBBBBB'
+    #cm = sns.color_palette("_r", as_cmap=True)
+    cm = sns.light_palette("firebrick", as_cmap=True)
+    #cm = sns.dark_palette("seagreen", as_cmap=True)
+
+    avg_part = avg_part[:14, :14]
+    fig, ax = plt.subplots(figsize=(12, 12))
+    sidey=np.arange(15)
+    X, Y = np.meshgrid(sidey, sidey)
+    # - 0.5 for X and Y to centre the data at the exciton numbers - this would start them at 0 though, so shift the tick labels by 1
+    im = ax.pcolormesh(X - 0.5, Y - 0.5, np.transpose(avg_part), cmap=cm, ec=edgecolour)
+    ax.set_yticks(np.arange(0, np.shape(avg_part)[0], dtype=int))
+    ax.set_yticklabels("{:5.0f}".format(eigval) for eigval in eigvals[:14])
+    #ax.set_yticks(np.arange(0, np.shape(avg_part)[0], dtype=int))
+    #ax.set_yticklabels([str(i + 1) for i in np.arange(0, np.shape(avg_part)[0], dtype=int)])
+    ax.set_xticks(np.arange(len(pigments[:14])))
+    ax.set_xlabel("Pigment")
+    ax.set_ylabel(r'$ \left< E_{\,\text{\huge{exciton}}} \right> (cm^{-1}) $')
+    ax.set_xticklabels(pigments[:14])
+    ax.xaxis.set_ticks_position('top')
+    ax.xaxis.set_label_position('top')
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="left", va="center", rotation_mode="anchor")
+    plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
+    # for some reason "location" does not work as a kw arg here. the matplotlib docs say it should. fucking useless shit
+    cax = plt.axes([0.125, 0.03, 0.675, 0.055])
+    cbar = fig.colorbar(im, cax=cax, orientation='horizontal')
+    cbar.set_label("Exciton participation")
+    sideax = plt.axes([0.82, 0.1, 0.09, 0.8])
+    #sideax.plot(exc_620_coupling[:14])
+    sidex=np.linspace(-0.5, 0.5, num=2, endpoint=True)
+    sidey=np.arange(15)
+    X, Y = np.meshgrid(sidex, sidey)
+    sideax.pcolormesh(X, Y, np.transpose(exc_620_coupling[np.newaxis, :14]), cmap=cm, ec=edgecolour)
+    sideax.set_xticks([])
+    sideax.set_yticks([])
+    #sideax.yaxis.set_ticks_position('right')
+    sideax.set_title(r'$ \left< J^{2}_{\text{\Large{exc}},\, \text{\Large{LUT620}}} \right>  $', pad=12.0, fontsize=28)
+    for i in range(len(exc_620_coupling[:14])):
+        sideax.text(0, i + 0.5, "{:3.1f}".format(exc_620_coupling[i]), ha="center", va="center", color="k")
+        
+    plt.savefig("{}/exciton_heatmap.pdf".format(path), bbox_inches='tight')
+
+        
+    kwargs = {
+        'cmap' : sns.color_palette("flare", as_cmap=True),
+        'ticklabels' : pigments,
+        'xlabel' : r'Pigment',
+        'ylabel' : r'Pigment',
+        'cbar_label' : r'RMSD $ (\text{\AA}) $',
+    }
+
+    rmsd = np.loadtxt("{}/rmsd_average.dat".format(path))
+    rmsd = np.ma.masked_where(rmsd < 0.1, rmsd)
+    plot_heatmap(rmsd, "{}/rmsd.pdf".format(path), kwargs=kwargs)
+
+    theta = np.loadtxt("{}/theta_average.dat".format(path))
+
+    plot_kwargs = {
+        'vmin' : 0.,
+        'vmax' : np.pi,
+    }
+
+    cbar_kwargs = {
+        'ticks' : [0., np.pi/4., np.pi/2., 3. * np.pi / 4., np.pi],
+    }
+
+    kwargs['cmap'] = "RdBu_r"
+    kwargs['cbar_ticklabels'] = [r'$ 0 $', r'$ \frac{\pi}{4} $', r'$ \frac{\pi}{2} $', r'$ \frac{3\pi}{4} $', r'$ \pi $']
+    kwargs['cbar_label'] = r'$ \theta $'
+    plot_heatmap(theta, "{}/theta.pdf".format(path), kwargs=kwargs, plot_kwargs=plot_kwargs, cbar_kwargs=cbar_kwargs)
+
+    jij = np.loadtxt("{}/jij_average.dat".format(path))
+    kwargs['cmap'] = "RdBu_r"
+    kwargs['cbar_label'] = r'$ J_{ij} (\text{cm}^{-1}) $'
+    plot_kwargs['vmin'] = -50.
+    plot_kwargs['vmax'] = +50.
+    del kwargs['cbar_ticklabels']
+    plot_heatmap(np.ma.masked_where(jij > 1000.0, jij), "{}/jij.pdf".format(path), kwargs=kwargs, plot_kwargs=plot_kwargs)